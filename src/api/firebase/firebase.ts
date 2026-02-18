3317ef2c89bd4ee6add05a651197eab24d04a44a
Fatima-malik7 <fatimamalikk77@gmail.com>
2026-02-18 10:44:59 +0500

    Firebase connection

--- src/api/firebase/firebase.ts
+++ src/api/firebase/firebase.ts
@@ -1,7 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -9,13 +8,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCAOJvb0wMJTD8TnK1MuMFH7-pmCT8VBPs",
-  authDomain: "cashflow-system-3ab2b.firebaseapp.com",
-  projectId: "cashflow-system-3ab2b",
-  storageBucket: "cashflow-system-3ab2b.firebasestorage.app",
-  messagingSenderId: "660308923185",
-  appId: "1:660308923185:web:1bc6e38e03bdd45b31d610",
-  measurementId: "G-C293HCL576"
+  apiKey: "AIzaSyBpHnEM6bjOnGYzUqQBSDZxJUEerBz3PjE",
+  authDomain: "cashflow-system-9088f.firebaseapp.com",
+  projectId: "cashflow-system-9088f",
+  storageBucket: "cashflow-system-9088f.firebasestorage.app",
+  messagingSenderId: "352663626458",
+  appId: "1:352663626458:web:1b3a038255cc72f5807be3",
+  measurementId: "G-1V5ZTH8JHX"
 };
 
 // Initialize Firebase
@@ -24,6 +23,3 @@ const analytics = getAnalytics(app);
 
 //authentication 
 export const auth = getAuth(app);
-
-// Initialize Cloud Firestore and get a reference to the service
-export const db = getFirestore(app, "default");
